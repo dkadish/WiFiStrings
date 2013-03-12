2cbbfddb1e6024e1a435503f3e9c7621df724d34
David Kadish <david.kadish@gmail.com>
2013-03-12 09:27:22 -0700

    First copy that works. Removed inheritance and just gave it an instance of Client

--- WiFiStringClient.h
+++ WiFiStringClient.h
@@ -11,24 +11,35 @@
 #include "WiFiClient.h"
 
 extern "C" {
-  #include "utility/wl_definitions.h"
-  #include "utility/wl_types.h"
-  #include "string.h"
-  #include "utility/debug.h"
+#include "utility/wl_definitions.h"
+#include "utility/wl_types.h"
+#include "string.h"
+#include "utility/debug.h"
 }
 
-class WiFiStringClient : public WiFiClient {
+class WiFiStringClient {
 public:
-	WiFiStringClient(uint8_t sock=MAX_SOCK_NUM);
+	WiFiStringClient(uint8_t sock = MAX_SOCK_NUM);
+	WiFiStringClient(WiFiClient& aClient, uint8_t sock = MAX_SOCK_NUM);
 
 	virtual size_t writeString(String);
 	virtual int available();
 	virtual String readString();
+	virtual int connect(IPAddress ip, uint16_t port) {
+		return iClient->connect(ip, port);
+	}
+	;
+	virtual int connect(const char *host, uint16_t port) {
+		return iClient->connect(host, port);
+	}
+	;
 
 	//friend class WiFiStringServer;
 	String partialMessage;
 	bool ffReceived;
 	char z;
+protected:
+	WiFiClient* iClient;
 private:
 	static const unsigned char lineEnding;
 };
